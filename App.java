853fb47fe23c037561669138a3eea6c808e5af86
Radoslaw Choluj <radekcholuj@wp.pl>
2020-12-11 22:23:25 +0100

    #1 przygotowanie tablic dla imienia oraz nazwiska, przygotowanie zmiennej wiek, przypisanie oraz wyświetlenie danych

--- App.java
+++ App.java
@@ -1,3 +1,62 @@
 class App{
-
+	public static void main(String args[]){
+		char[] name = new char [5];
+		name[0] = 'K';
+		name[1] = 'A';
+		name[2] = 'R';
+		name[3] = 'O';
+		name[4] = 'L';
+		
+		
+		char[] surname = new char [11];
+		surname[0] = 'M';
+		surname[1] = 'I';
+		surname[2] = 'C';
+		surname[3] = 'H';
+		surname[4] = 'N';
+		surname[5] = 'I';
+		surname[6] = 'E';
+		surname[7] = 'W';
+		surname[8] = 'I';
+		surname[9] = 'C';
+		surname[10] = 'Z';
+		
+		
+		int age = 22;
+		
+		System.out.println("-----------------------");
+		System.out.print("Imiê: ");
+		System.out.print(name[0]);
+		System.out.print(name[1]);
+		System.out.print(name[2]);
+		System.out.print(name[3]);
+		System.out.println(name[4]);
+		
+		
+		
+		System.out.print("Nazwisko: ");
+		System.out.print(surname[0]);
+		System.out.print(surname[1]);
+		System.out.print(surname[2]);
+		System.out.print(surname[3]);
+		System.out.print(surname[4]);
+		System.out.print(surname[5]);
+		System.out.print(surname[6]);
+		System.out.print(surname[7]);
+		System.out.print(surname[8]);
+		System.out.print(surname[9]);
+		System.out.print(surname[10]);
+		System.out.println(" ["+surname.length+"] ");
+		
+		System.out.println("Wiek: "+age);
+		
+		
+		System.out.println("-----------------------");
+		
+		
+		
+		
+		
+	}
+	
 }
