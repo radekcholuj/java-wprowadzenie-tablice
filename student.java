2408e940ea60a14f877ec7542e1fcb2a791cc8f7
Radoslaw Choluj <radekcholuj@wp.pl>
2020-12-12 22:59:33 +0100

    Dodanie informacji o studencie i ich wyśiwtlenie.

---
+++ student.java
@@ -0,0 +1,16 @@
+public class student {
+    public static void main(String[] args) {
+        int index = 253697;
+        double[] courseGradesIT = new double[]{5, 1, 3.5, 1, 6, 4.5};
+
+        System.out.println("numer indexu: " + index);
+        System.out.print("oceny z przedmiotu informatyka: ");
+        System.out.print(", " + courseGradesIT[0]);
+        System.out.print(", " + courseGradesIT[1]);
+        System.out.print(", " + courseGradesIT[2]);
+        System.out.print(", " + courseGradesIT[3]);
+        System.out.print(", " + courseGradesIT[4]);
+        System.out.print(", " + courseGradesIT[5]);
+
+    }
+}
