0d6e68e1983ef3fecf3d2b6430b4b506bb171ac7
Radoslaw Choluj <radekcholuj@wp.pl>
2020-12-12 22:55:31 +0100

    Dodanie kodów pocztowych miasta Świnoujście i wyświetlenie ich.

---
+++ postalcodes.java
@@ -0,0 +1,16 @@
+public class postalcodes {
+    public static void main(String[] args) {
+        int[] postalCodes = new int[6];
+
+        postalCodes[0] = 72600;
+        postalCodes[1] = 72601;
+        postalCodes[2] = 72602;
+        postalCodes[3] = 72603;
+        postalCodes[4] = 72604;
+        postalCodes[5] = 72605;
+
+        System.out.println("Kody pocztowe miejscowości Świnoujście");
+        System.out.print(postalCodes[0]+", "+postalCodes[1]+", "+postalCodes[2]+", "+postalCodes[3]+", " +
+                ","+postalCodes[3]+", "+postalCodes[4]+", "+postalCodes[5]);
+    }
+}
