a0e7f12b8f1723483aaf20454fa97b58be1087fd
Radoslaw Choluj <radekcholuj@wp.pl>
2020-12-12 23:08:07 +0100

    Dodanie 5 ocen i wyświetlenie 3 ostatnich.

---
+++ gradesMATH.java
@@ -0,0 +1,33 @@
+public class gradesMATH {
+    public static void main(String[] args) {
+        int[] gradesMath = new int[5];
+        int temp;
+
+        gradesMath[0] = 1;
+        gradesMath[1] = 2;
+        gradesMath[2] = 3;
+        gradesMath[3] = 1;
+        gradesMath[4] = 2;
+
+
+        System.out.println("Ostatnie trzy oceny to: ");
+        System.out.print(gradesMath[2] + ", ");
+        System.out.print(gradesMath[3] + ", ");
+        System.out.print(gradesMath[4] + ", ");
+
+        gradesMath[1] = 5;
+        System.out.println("");
+
+        temp = gradesMath[1];
+        gradesMath[1] = gradesMath[4];
+        gradesMath[4] = temp;
+        temp = gradesMath[1];
+        gradesMath[1] = gradesMath[3];
+        gradesMath[3] = temp;
+        temp = gradesMath[2];
+        gradesMath[2] = gradesMath[3];
+        gradesMath[3] = temp;
+
+    }
+
+}
